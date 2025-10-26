7dd5a019cb9e0bd4466498c6b44f535dcfc13dfb
HamzaIT2 <hmzhkymr4@gmail.com>
2025-10-27 00:33:16 +0300

    fix: use DATABASE_URL for Render

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -5,16 +5,17 @@ export const getDatabaseConfig = (
   configService: ConfigService,
 ): TypeOrmModuleOptions => ({
   type: 'postgres',
-  host: configService.get<string>('DB_HOST','localhost'),
+  host: configService.get<string>('DB_HOST','dpg-d3v7j6re5dus73a6i67g-a.frankfurt-postgres.render.com'),
   port: configService.get<number>('DB_PORT',5432),
-  username: configService.get<string>('DB_USERNAME','postgres'),
-  password: configService.get<string>('DB_PASSWORD','root'),
-  database: configService.get<string>('DB_DATABASE','iraq-marketplace'),
+  username: configService.get<string>('DB_USERNAME','postgre'),
+  password: configService.get<string>('DB_PASSWORD','9RaUdujath6QWcWwvs9KiwdLd7V7HuGP'),
+  database: configService.get<string>('DB_DATABASE','iraqmarketplace'),
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-  synchronize: configService.get<string>('NODE_ENV','NODE_ENV') === 'development',
-  logging: configService.get<string>('NODE_ENV','NODE_ENV') === 'development',
+  synchronize: configService.get<string>('NODE_ENV','NODE_ENV') === 'production',
+  logging: configService.get<string>('NODE_ENV','NODE_ENV') === 'production',
   ssl:{ rejectUnauthorized: false },
   extra: {
     max: 10,
   },
 });
+//development
