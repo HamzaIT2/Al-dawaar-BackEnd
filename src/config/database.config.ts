45d1a37b609a96273f28c5f087ebdc2689282a4b
HamzaIT2 <hmzhkymr4@gmail.com>
2025-10-26 22:28:06 +0300

    update

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -6,14 +6,14 @@ export const getDatabaseConfig = (
 ): TypeOrmModuleOptions => ({
   type: 'postgres',
   host: configService.get<string>('DB_HOST','localhost'),
-  port: configService.get<number>('DB_PORT',5432),
+  port: configService.get<number>('DB_PORT',3000),
   username: configService.get<string>('DB_USERNAME','postgres'),
   password: configService.get<string>('DB_PASSWORD','root'),
   database: configService.get<string>('DB_DATABASE','iraq-marketplace'),
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   synchronize: configService.get<string>('NODE_ENV','NODE_ENV') === 'development',
   logging: configService.get<string>('NODE_ENV','NODE_ENV') === 'development',
-  ssl: { rejectUnauthorized: false },
+  ssl: false,
   extra: {
     max: 10,
   },
